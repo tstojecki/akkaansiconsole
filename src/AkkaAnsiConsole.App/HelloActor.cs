7df08893c8b791ced4be89806413c38071a79a2d
tstojecki <tstojecki@gmail.com>
2023-03-17 17:37:37 +0100

    initial commit

---
+++ src/AkkaAnsiConsole.App/HelloActor.cs
@@ -0,0 +1,34 @@
+﻿using static LiveConsoleActor;
+
+namespace AkkaAnsiConsole.App;
+
+public class HelloActor : ReceiveActor
+{
+    private readonly ILoggingAdapter _log = Context.GetLogger();
+    private int _helloCounter = 0;
+    
+    private IActorRef? subscriber = null;
+    
+    public HelloActor()
+    {
+        Receive<string>(message =>
+        {
+            var msg = $"{message} {_helloCounter++}";
+
+            if (subscriber != null) 
+            {
+                subscriber?.Tell(msg);
+                return;
+            }
+            
+           _log.Info(msg);
+        });
+
+        Receive<SubscribeActor>(x =>
+        {
+            subscriber = x.Ref;
+        });
+    }
+
+    public record SubscribeActor(IActorRef Ref);
+}
---
+++ src/AkkaAnsiConsole.App/Program.cs
@@ -0,0 +1,60 @@
+﻿using Akka.Hosting;
+using AkkaAnsiConsole.App;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Spectre.Console;
+
+var hostBuilder = new HostBuilder();
+
+hostBuilder.ConfigureServices((context, services) =>
+{
+    services.AddAkka("MyActorSystem", (builder, sp) =>
+    {
+        builder
+            .WithActors((system, registry, resolver) =>
+            {
+                var helloActor = system.ActorOf(Props.Create(() => new HelloActor()), "hello-actor");
+                registry.Register<HelloActor>(helloActor);
+            })
+            .WithActors((system, registry, resolver) =>
+            {
+                var timerActorProps =
+                    resolver.Props<TimerActor>(); // uses Msft.Ext.DI to inject reference to helloActor
+                var timerActor = system.ActorOf(timerActorProps, "timer-actor");
+                registry.Register<TimerActor>(timerActor);
+            });
+    });
+});
+
+var host = hostBuilder.Start();
+
+var actorSystem = host.Services.GetRequiredService<ActorSystem>();
+var actorRegistry = host.Services.GetRequiredService<ActorRegistry>();
+var helloActor = await actorRegistry.GetAsync<HelloActor>();
+
+var table = new Table().Centered();
+AnsiConsole.Live(table)
+    .Start(ctx =>
+    {
+        var liveConsoleActor = actorSystem.ActorOf(Props.Create(() => new LiveConsoleActor(table, ctx)));
+        helloActor.Tell(new HelloActor.SubscribeActor(liveConsoleActor));
+
+        while (true)
+        {
+            Thread.Sleep(1000);
+        }
+    });
+
+await host.WaitForShutdownAsync();
+
+public class LiveConsoleActor : ReceiveActor
+{
+    public LiveConsoleActor(Table table, LiveDisplayContext liveDisplayContext)
+    {
+        Receive<string>(message =>
+        {
+            table.AddColumn(message);
+            liveDisplayContext.Refresh();
+        });
+    }
+}
